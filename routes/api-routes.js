79f5e34cf82859cbb4fed01ea850dfde3ae71ce9
JES20 <dore.joel.dore@gmail.com>
2021-02-03 13:44:47 -0500

    Add GET /workouts route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,24 @@
 const router = require('express').Router()
 const db = require('../models')
 
+router.get('/workouts', (req, res) => {
+    db.Workout.find({})
+        .then(data => {
+            console.log(data.length, "records found!")
+            res.send(data)
+        })
+})
 
+router.put('/workouts', (req, res) => {
+
+})
+
+router.post('/workouts', (req, res) => {
+
+})
+
+router.get('/workouts/range', (req, res) => {
+
+})
 
 module.exports = router
