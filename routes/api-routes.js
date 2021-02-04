408a797c4250930d438fabb516f1ecd6ca244603
JES20 <dore.joel.dore@gmail.com>
2021-02-04 18:36:37 -0500

    Add POST /workouts to create a new workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -18,7 +18,14 @@ router.put('/workouts', (req, res) => {
 })
 
 router.post('/workouts', (req, res) => {
-
+    db.Workout.create(req.body)
+        .then(data => {
+            console.log("1 record inserted!")
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
 })
 
 router.get('/workouts/range', (req, res) => {
